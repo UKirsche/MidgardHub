831ef5239e53c9aea5ddff8fd7a059d270bed949
ukirsche <ukirschenmann@gmx.de>
2018-03-26 13:09:47 +0200

    GUI verbessert

--- HubGui.cs
+++ HubGui.cs
@@ -10,14 +10,14 @@ public class HubGui : MonoBehaviour
 		public bool main = false;
 		public bool scenes = false;
 		public bool dice = false;
-		public bool fight = false;
+		public bool generate = false;
 
 		public void setMenusFalse ()
 		{
 			this.main = false;
 			this.scenes = false;
 			this.dice = false;
-			this.fight = false;
+			this.generate = false;
 		}
 	}
 
@@ -32,17 +32,17 @@ public class HubGui : MonoBehaviour
 		Main,
 		Scenes,
 		Dice,
-		Fight
+		Generator
 	}
 
 	public GUISkin Skin;
 
 	private Vector2 scrollPos = new Vector2 ();
 
-	private string buttonTextMain = "Erläuterung";
-	private string buttonTextSzenen = "Szenen";
+	private string buttonTextMain = "Howto";
+	private string buttonTextSzenen = "Abenteuer";
 	private string buttonTextWuerfelDeck = "Würfeldeck";
-	private string buttonTextKampf = "Kampfgitter";
+	private string buttonTextGenerator = "Generator";
 
 
 
@@ -50,11 +50,11 @@ public class HubGui : MonoBehaviour
 									 "Das Hauptmenü erlaubt v.a. das Auswählen von Szenerien sowie das Öffnen des Würfeldecks.\n" +
 									 "Aus den Szenerien wählen die Spieler die vom Meister angegebene Szene.\n" +
 									 "Im Würfeldeck können die Teilnehmer ihren gewünschten Würfel auswählen und für alle sichtbar würfeln.\n\n\n" +
-	                                 "<color=black>Szenen</color>\t\t Lädt ein Level, gemeinsam erkundet werden kann\n" +
+	                                 "<color=black>Abenteuer</color>\t Lädt das Abenteuer, das erkundet werden kann, evtl auch eine Zusammenfassung\n" +
 	                                 "<color=black>Würfeldeck</color>\t Lädt den gemeinesamen Würfelbecher\n" +
-	                                 "<color=black>Kampf</color>\t\t Kampfgitter zur taktischen Darstellung von Kämpfen\n";
+	                                 "<color=black>Generator</color>\t Erlaubt das Erstellen eines Midgard Characters. Fürs Spiel kann Avatar gewählt werden\n";
 
-	private string sceneDescription = "Wähle eine Szenerie aus dem Abenteur";
+	private string sceneDescription = "Wähle das Abenteuer";
 	private string diceDescription = "Das Würfeldeck erlaubt für alle sichtbares Würfeln";
 
 
@@ -76,7 +76,7 @@ public class HubGui : MonoBehaviour
 		}
 
 		scene1Button = new MenuBtn () { 
-			Text = "<color=black>Umland Cambrygg</color>", 
+			Text = "<color=black>Mord im Kloster</color>", 
 			Link = "MidgardGame-Scene" 
 		};
 
@@ -126,6 +126,10 @@ public class HubGui : MonoBehaviour
 			choseMenu (MenuType.Dice);
 		}
 
+		if (GUILayout.Button (buttonTextGenerator)) {
+			choseMenu (MenuType.Generator);
+		}
+
 		GUILayout.EndScrollView ();
 		#endregion
 
@@ -153,30 +157,29 @@ public class HubGui : MonoBehaviour
 		//Zeige  nur gewählte Elemente in der rechten Gruppe
 		if (this.menuSelection.main == true) {
 			GUILayout.Label (mainDescription);
-		}
-		else
-			if (this.menuSelection.scenes == true) {
-				GUILayout.Label (sceneDescription);
-				if (GUILayout.Button (this.scene1Button.Text)) {
-					SceneManager.LoadScene (this.scene1Button.Link);
-				}
-				if (GUILayout.Button (this.scene2Button.Text)) {
-					SceneManager.LoadScene (this.scene2Button.Link);
-				}
+		} else if (this.menuSelection.scenes == true) {
+			GUILayout.Label (sceneDescription);
+			if (GUILayout.Button (this.scene1Button.Text)) {
+				SceneManager.LoadScene (this.scene1Button.Link);
 			}
-			else
-				if (this.menuSelection.dice == true) {
-					GUILayout.Label (diceDescription);
-					if (GUILayout.Button (this.wuerfelButton.Text)) {
-						SceneManager.LoadScene (this.wuerfelButton.Link);
-					}
-					if (GUILayout.Button (this.charGenButton.Text)) {
-						SceneManager.LoadScene (this.charGenButton.Link);
-					}
-					if (GUILayout.Button (this.charChoserButton.Text)) {
-						SceneManager.LoadScene (this.charChoserButton.Link);
-					}
+			if (GUILayout.Button (this.scene2Button.Text)) {
+				SceneManager.LoadScene (this.scene2Button.Link);
+			}
+		} else if (this.menuSelection.dice == true) { 
+				GUILayout.Label (diceDescription);
+				if (GUILayout.Button (this.wuerfelButton.Text)) {
+					SceneManager.LoadScene (this.wuerfelButton.Link);
 				}
+		
+		} else if(this.menuSelection.generate==true){
+			if (GUILayout.Button (this.charGenButton.Text)) {
+				SceneManager.LoadScene (this.charGenButton.Link);
+			}
+			if (GUILayout.Button (this.charChoserButton.Text)) {
+				SceneManager.LoadScene (this.charChoserButton.Link);
+			}
+		}
+				
 	}
 
 	/// <summary>
@@ -196,8 +199,8 @@ public class HubGui : MonoBehaviour
 		case MenuType.Dice: 
 			this.menuSelection.dice = true;
 			break;
-		case MenuType.Fight: 
-			this.menuSelection.fight = true;
+		case MenuType.Generator: 
+			this.menuSelection.generate = true;
 			break;
 		default:
 			break;
