3d46b63bdd11a110402971f66f3fa6b59e420c5d
Uwe Kirschenmann <ukirschenmann@gmx.de>
2018-03-27 15:27:13 +0200

    Add files via upload

---
+++ Ideen MiCoRo weiter.mm
@@ -0,0 +1,171 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1522147975405" ID="ID_1794263701" MODIFIED="1522156770077" TEXT="Ideen MiCoRo weiter">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hier nochmal: Regelwerk &#252;bberlesen, was man am besten umsetzen kann
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1522148013308" ID="ID_601180065" MODIFIED="1522154983614" POSITION="right" TEXT="rundenbasiertes Kampfsystem">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      das rundenbasierte Kampfsystem soll sich am Midgard-Kampfsystem orientieren. Dabei g&#228;be es die M&#246;glichkeit des System komplett umzusetzen oder rudiment&#228;r
+    </p>
+    <p>
+      Vereinfachte Idee w&#228;re:
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      - Kampf l&#228;uft ab mit Animationen: Ergebnisse werden &#252;ber Text eingeblendet (Bsp: Wolf attackiert Tarkin: 3AP weg)
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      - Bedeutet HUD f&#252;r Lebensenergie und Waffen
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      - Todo: Ziehe Waffe - Animation, Pause Game usw.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node CREATED="1522148238138" ID="ID_272413637" MODIFIED="1522153773846" TEXT="2D-Tilemap">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Tilemap-Version w&#228;re nur ab unity 2017 zu verwirklichen. https://www.youtube.com/watch?v=4zO5ab05fVM als Tutorial interessant.
+    </p>
+    <p>
+      Frage: Mit AWW besser?
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Eignet sich f&#252;r komplexe Kampfsimulation a la Tabletop
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Vereinfachung w&#228;re: AI der Angreifer bzw. im Bedarfsfall Spielleiter
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1522153323016" ID="ID_946009036" MODIFIED="1522153332798" TEXT="Unity 2017 3">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1522153336368" ID="ID_878127795" MODIFIED="1522153770539" TEXT="Vereinfachung Spielleiter">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spielleiter kann w&#228;hlen ob AI oder selber zu steuern...
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1522153773846" ID="ID_1558161172" MODIFIED="1522153846837" TEXT="Vereinfachung Spieler">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spieler kann ebenfalls selbst&#228;ndig k&#228;mpfen lassen, falls er will
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1522148416875" ID="ID_1719771803" MODIFIED="1522148462780" TEXT="MidgardCharacter-Anbindug">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      MidgardCharacter muss angebunden sein. am Besten durch eigenes Skript
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1522148466858" ID="ID_528132663" MODIFIED="1522149759413" TEXT="3D-Einbindung">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hier pausiert das Geschehen, sobald sich ein Kampf anbahnt.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1522154986670" ID="ID_1702554385" MODIFIED="1522155230159" TEXT="nicht grafisch: nur text">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      einfachste Form: Daten der NPCs sowie Spielerdaten und Regelmachenismen m&#252;ssen da sein.
+    </p>
+    <p>
+      W&#228;re wohl erstmal die beste Option!
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1522155238366" ID="ID_150526419" MODIFIED="1522155267711" POSITION="left" TEXT="Weitere Manipulation von Objekten">
+<node CREATED="1522155253999" ID="ID_805928967" MODIFIED="1522155257933" TEXT="&#xdc;ber Mausklick"/>
+<node CREATED="1522155267711" ID="ID_1640542117" MODIFIED="1522155273198" TEXT="Entsprechende Animation"/>
+</node>
+<node CREATED="1522155410734" ID="ID_1831854169" MODIFIED="1522155452890" POSITION="right" TEXT="Fertigkeiten anwenden">
+<node CREATED="1522155434743" ID="ID_1542014875" MODIFIED="1522155449191" TEXT="Beobachten usw."/>
+<node CREATED="1522155452890" ID="ID_1222648297" MODIFIED="1522156754254" TEXT="Automatische Fertigkeiten"/>
+</node>
+</node>
+</map>
